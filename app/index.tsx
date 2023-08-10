0a0b0654336fc8b8b057e6e1d7fb35587b6fe7b7
Jancer Lima <contato.jancersl@gmail.com>
2023-08-09 21:15:48 -0300

    Expo router

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+export default function Page() {
+  return (
+    <View style={{ height: "100%", justifyContent: "center", alignItems: "center" }}>
+      <Text>Olá mestres!!!</Text>
+      <Text>Olá mestres!!!</Text>
+      <Text>Olá mestres!!!</Text>
+      <Text>Olá mestres!!!</Text>
+    </View>
+  );
+}
