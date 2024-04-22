fb132140fdb234dc148f0e205612f34f3094a7a9
Jancer Lima <contato.jancersl@gmail.com>
2024-04-22 20:27:30 -0300

    Expo secure store

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,61 @@
-import { Link } from "expo-router";
-import { View, Text, StyleSheet, TextInput } from "react-native";
-import { EInterFonts } from "../../src/shared/inter-fonts.enum";
-import { EPoppinsFonts } from "../../src/shared/poppins-fonts.enum";
+import { Button, StyleSheet, TextInput, View } from "react-native";
+import { setItemAsync, getItemAsync, deleteItemAsync } from "expo-secure-store";
 
 export default function HomeTab() {
+  const KEY = "key";
+
+  const save = async (key: string, value: string) => {
+    await setItemAsync(key, value);
+
+    alert("Item salvo com sucesso");
+  };
+
+  const readValue = async (key: string) => {
+    const result = await getItemAsync(key);
+
+    if (result) {
+      alert(`Item referente a ${key} é ${result}`);
+    } else {
+      alert("Item não encontrado");
+    }
+  };
+
+  const deleteValue = async (key: string) => {
+    await deleteItemAsync(key);
+
+    alert("Item removido com sucesso");
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 40, fontFamily: EPoppinsFonts.BOLD }}>
+      {/* <Text style={{ fontSize: 40, fontFamily: EPoppinsFonts.BOLD }}>
         Hello from Home
       </Text>
       <Link href={"profile"}>Perfil</Link>
-      <Link href={"edit"}>Edit</Link>
+      <Link href={"edit"}>Edit</Link> */}
+
+      <TextInput
+        style={styles.input}
+        onSubmitEditing={(event) => {
+          save(KEY, event.nativeEvent.text);
+        }}
+        placeholder="Digite um valor para salvar"
+      />
+
+      <View style={styles.buttonsContainer}>
+        <Button
+          title="Ler valor"
+          onPress={() => {
+            readValue(KEY);
+          }}
+        />
+        <Button
+          title="Apagar valor"
+          onPress={() => {
+            deleteValue(KEY);
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -27,6 +72,11 @@ const styles = StyleSheet.create({
     width: 300,
     padding: 5,
     borderRadius: 10,
-    marginTop: 20,
+    marginBottom: 20,
+  },
+  buttonsContainer: {
+    alignItems: "center",
+    justifyContent: "space-between",
+    height: "15%",
   },
 });
