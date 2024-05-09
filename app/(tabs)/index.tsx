931d22532fc99d4cdf7064566249765a6bdcd7bf
Jancer Lima <contato.jancersl@gmail.com>
2024-05-08 22:36:21 -0300

    Clipboard string

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,26 @@
-import { StyleSheet, View } from "react-native";
-import WebView from "react-native-webview";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as Clipboard from "expo-clipboard";
 
 export default function HomeTab() {
+  const pixCopyPaste = "198749549813584935.gov.br";
+
+  const handleCopyKey = async () => {
+    await Clipboard.setStringAsync(pixCopyPaste);
+
+    // Dar feedback
+  };
+
+  const handleCopyText = async () => {
+    const text = await Clipboard.getStringAsync();
+
+    console.log(text);
+  };
+
   return (
-    <View style={{ height: 200 }}>
-      <WebView
-        source={{
-          uri: "https://www.mestresdaweb.com.br",
-        }}
-      />
+    <View style={styles.container}>
+      <Text>Chave pix: {pixCopyPaste}</Text>
+      <Button title="Copiar chave" onPress={handleCopyKey} />
+      <Button title="Colar texto" onPress={handleCopyText} />
     </View>
   );
 }
@@ -16,5 +28,7 @@ export default function HomeTab() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
